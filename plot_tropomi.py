313d3c6bfeaefe2b36d0ca19f4eca0dacf45c6a3
Rica Cruz <rcruz@carbon.atmosp.physics.utoronto.ca>
2020-05-21 12:33:36 -0400

    Renamed plot_tropomi.py to plot_tropomi_deprecated.py and plot_toronto.py to plot_tropomi.py

--- plot_tropomi.py
+++ plot_tropomi.py
@@ -1,106 +1,37 @@
-### Importing libraries
-import glob
-import netCDF4 as nc 
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-from mpl_toolkits.basemap import Basemap
-import numpy as np 
-import numpy.ma as ma
-import sys
+# Preamble
+import numpy as np
+import xarray as xr
+import netCDF4 as nc
+from glob import iglob
+from os.path import join
+from collections import namedtuple
+import tropomi_functions as tf
 
-# Remove deprecation warnings
-import warnings
-warnings.filterwarnings("ignore", category=UserWarning)
+# Variables
+file_name = '/export/data/scratch/tropomi/no2/S5P_OFFL_L2__NO2____20200505T171512_20200505T185642_13270_01_010302_20200507T092201.nc'
+sds_name = 'nitrogendioxide_tropospheric_column'
 
-# #This finds the user's current path so that all hdf4 files can be found
-# try:
-#     # inventory.txt must be in the same directory as plot_tropomi.py
-#     fileList=open('inventory.txt','r')
-# except:
-#     print('Did not find a text file containing file names (perhaps name does not match)')
-#     sys.exit()
+# Define plot extent centred around Toronto
+Point = namedtuple('Point', 'lon lat')
+toronto_coords = Point(-79.3832, 43.6532)
 
-# # Create an empty list
-# ncfiles = []
+extent_size = 5
+plot_limits = (toronto_coords.lon-extent_size,
+               toronto_coords.lon+extent_size,
+               toronto_coords.lat-extent_size,
+               toronto_coords.lat+extent_size)
+# print("Plot extent: lon({}, {}), lat({}, {})".format(*plot_limits))
 
-# # Iterate over the files in the no2 directory and append them to the list
-# for file in glob.glob("/export/data/scratch/tropomi/no2/*.nc"):
-#     ncfiles.append(file)
 
-# my_example_nc_file = ncfiles[1]
+with xr.open_dataset(file_name, group='/PRODUCT')[sds_name] as no2tc:
+    # Reading the data
+    fields, short_file_name = tf.read_data(file_name)
+    
+    # Print information about orbit 
+    print("Orbit: {}, Sensing Start: {}, Sensing Stop: {}".format(fields[10],
+                                                              fields[8],
+                                                              fields[9]))
 
-#This finds the user's current path so that all hdf4 files can be found
-try:
-    fileList=open('inventory.txt','r')
-except:
-    print('Did not find a text file containing file names (perhaps name does not match)')
-    sys.exit()
-
-# test_file = '/export/data/scratch/tropomi/no2/S5P_OFFL_L2__NO2____20200505T171512_20200505T185642_13270_01_010302_20200507T092201.nc'
-for test_file in fileList:
-    test_file=test_file.strip()
-    short_file_name=test_file[33:]
-    user_input=input('\nWould you like to create a map of\n' + test_file + '\n\n(Y/N/exit)')
-    if(user_input == 'N' or user_input == 'n'):
-        print('Skipping...')
-    elif(user_input == 'exit'):
-        sys.exit('The script has terminated at the user\'s request.')
-    else:
-        file = nc.Dataset(test_file, 'r')
-        # read the data
-        ds = file
-        grp = 'PRODUCT'
-        lons = ds.groups[grp].variables['longitude'][0][:][:] # longitude
-        lats = ds.groups[grp].variables['latitude'][0][:][:] # latitude
-        sds_name='nitrogendioxide_tropospheric_column'
-        map_label='mol/m2'
-        data = ds.groups[grp].variables[sds_name][0,:,:]
-
-        # ### FROM THE TUTORIAL ONLINE -- Stereographic Projection
-        # # Get some parameters for the Stereographic Projection
-        # lon_0 = lons.mean()
-        # lat_0 = lats.mean()
-        # # m = Basemap(width=5000000,height=3500000,
-        # #             resolution='l',projection='stere',\
-        # #             lat_ts=40,lat_0=lat_0,lon_0=lon_0)
-
-        # FROM READ_AND_MAP_TROPOMI_NO2_AI.PY -- cylindrical projection
-        m = Basemap(projection='cyl', resolution='l',
-                    llcrnrlat=-90, urcrnrlat = 90,
-                    llcrnrlon=-180, urcrnrlon = 180)
-
-        xi, yi = m(lons, lats)
-        map_title = 'Tropospheric vertical column of nitrogen dioxide'
-
-        # Choose plot colour
-        # Note: values are logarithmically scaled
-        cs = m.pcolor(xi,yi,np.squeeze(data),
-                      norm=LogNorm(vmin=10e-11, 
-                                   vmax=10e-3), 
-                      cmap='jet')
-
-        # Add Grid Lines
-        m.drawparallels(np.arange(-90., 120., 30.), labels=[1, 0, 0, 0])
-        m.drawmeridians(np.arange(-180, 180., 45.), labels=[0, 0, 0, 1])
-
-        # Add Coastlines and Country Boundaries
-        m.drawcoastlines(linewidth=0.5)
-        m.drawcountries()
-
-        # Add Colorbar
-        cbar = m.colorbar(cs, location='bottom', pad="10%")
-        cbar.set_label("mol/$\mathdefault{m^2}$")
-
-        # Add Title
-        plt.title('{0}\n {1}'.format(short_file_name, map_title))
-        plt.autoscale()
-        fig = plt.gcf()
-        # Show the plot window
-        plt.show()
-        is_save=str(input('\nWould you like to save this map? Please enter Y or N \n'))
-        if is_save == 'Y' or is_save == 'y':
-            # Saves plot as a png in the tropomi_figures folder
-            pngfile = '{0}.png'.format('world_figures/' + short_file_name[:-3])
-            fig.savefig(pngfile, dpi = 300)
-        #Close the hdf4 file
-        file.close()
+    # Plot orbit on the globe or on Toronto
+    # If Toronto, append plot_limits to plot_no2 args
+    tf.plot_no2(no2tc, 'globe', fields, short_file_name)
